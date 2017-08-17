2a74cc646784293d1b17f77df1e0d33e0ce7bfe1
Hack Reactor Student <student@hackreactor.com>
2017-08-16 19:13:59 -0500

    finished part1 and linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var head = 0;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+      if (list.head.next === null) {
+        list.head.next = Node(value);
+      }
+      list.tail.next = Node(value);
+      list.tail = Node(value);
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    //console.log(list.head,"here");
+    var ret = list.head.value;
+    list.head = list.head.next;
+    return ret;
   };
 
   list.contains = function(target) {
+    return has(list.head, target);
   };
 
   return list;
@@ -22,6 +41,16 @@ var Node = function(value) {
   node.next = null;
 
   return node;
+};
+
+var has = function(obj,target) {
+  if (obj.value === target) {
+    return true;
+  }
+  if (obj.next === null) {
+    return false;
+  }
+  return has(obj.next, target);
 };
 
 /*
